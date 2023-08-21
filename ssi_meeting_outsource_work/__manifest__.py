03d87d7da2d241ed0089fb49d99fa0a5ac965a67
mikevhe18 <viriyananda.michael@gmail.com>
2023-08-21 09:10:36 +0700

    [ssi_meeting_outsource_work] Change License To AGPL-3

--- ssi_meeting_outsource_work/__manifest__.py
+++ ssi_meeting_outsource_work/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Meeting + Outsource Work Integration",
     "version": "14.0.1.0.1",
     "website": "https://simetri-sinergi.id",
-    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
-    "license": "LGPL-3",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
     "installable": True,
     "depends": ["ssi_outsource_work", "calendar"],
     "data": [],
